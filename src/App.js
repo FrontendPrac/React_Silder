105caa94816570cce0a2751ed5857a21476e0015
Kidongg <dlrlehd7@daum.net>
2022-12-25 10:39:46 +0900

    Add slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function App() {
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+import styled from "styled-components";
+
+import logo from "./logo.png";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
   );
 }
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+class App extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      vertical: true,
+      verticalSwiping: true,
+    };
+    return (
+      <>
+        <StContainer>
+          <Slider {...settings}>
+            <StImg src={logo}></StImg>
+            <StImg src={logo}></StImg>
+            <StImg src={logo}></StImg>
+          </Slider>
+        </StContainer>
+      </>
+    );
+  }
+}
+
+const StContainer = styled.div`
+  border: 1px solid black;
+  margin: auto;
+  width: 90%;
+`;
+
+const StImg = styled.img`
+  margin: 10px;
+  width: 250px;
+  height: 400px;
+`;
 
 export default App;
